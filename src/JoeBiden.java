789ad39f0a7b97346b02209c526a283cdf9947a3
BT_1N3_06 <sample@bths.edu>
2024-10-25 10:34:15 -0400

    no more toilet

--- src/JoeBiden.java
+++ src/JoeBiden.java
@@ -3,6 +3,5 @@ public class JoeBiden {
         System.out.println("I like my cheese drippy bruh.");
         System.out.println("I'm in the thick of it and everybody knows.");
         System.out.println("From the screen, to the ring, to the pen, to the king.");
-        System.out.println("skibidi toilet");
     }
 }
