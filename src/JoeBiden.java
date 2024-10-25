622d4c02ef5db8934b2e5f031fda2fe366026253
BT_1N3_06 <sample@bths.edu>
2024-10-25 10:03:49 -0400

    sick lyrics 2.0

--- src/JoeBiden.java
+++ src/JoeBiden.java
@@ -2,5 +2,6 @@ public class JoeBiden {
     public static void main(String[] args) {
         System.out.println("I like my cheese drippy bruh.");
         System.out.println("I'm in the thick of it and everybody knows.");
+        System.out.println("From the screen, to the ring, to the pen, to the king.");
     }
 }
